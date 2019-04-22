2bbb0accf4be17f5fd88cfccf4ec36953be8c87e
crystalattice <crystalattice@gmail.com>
2019-04-22 12:43:16 -0500

    Added train row deletion function and train status function

--- db_setter_procs.py
+++ db_setter_procs.py
@@ -22,3 +22,6 @@ set_stations_proc = VoltProcedure(client, "Insert_Stations", [FastSerializer.VOL
                                                               FastSerializer.VOLTTYPE_STRING])
 set_rfid_proc = VoltProcedure(client, "Insert_RFID", [FastSerializer.VOLTTYPE_STRING,
                                                       FastSerializer.VOLTTYPE_STRING])
+del_trains_proc = VoltProcedure(client, "Delete_Train", [FastSerializer.VOLTTYPE_STRING])
+set_speed_proc = VoltProcedure(client, "Update_Speed", [FastSerializer.VOLTTYPE_INTEGER,
+                                                        FastSerializer.VOLTTYPE_STRING])

55b41b2ec9eb812a8121a4a3d29a8084edd0f99b
crystalattice <crystalattice@gmail.com>
2019-05-13 12:33:20 -0500

    Added default values

--- update_train_status.py
+++ update_train_status.py
@@ -7,16 +7,16 @@ from voltdbclient import *
 client = FastSerializer("localhost", 21212)
 
 
-def update_train_status(car, new_location, new_speed, new_status):
+def update_train_status(car="Engine", new_location="Station 1", new_speed, new_status=True):
     del_train_row.del_train(car)
     set_trains_proc.call([car, new_location, new_speed, new_status])
 
 
 if __name__ == "__main__":
-    car_id = input("Name of car/engine id: ")
-    location = input("Location: ")
+    car_id = input("Name of car/engine id: (Default = Engine")
+    location = input("Location: (Default = Station 1")
     speed = int(input("How fast? "))
-    status = input("Train status: ")
+    status = input("Train status: (Default = True)")
     update_train_status(car_id, location, speed, status)
 
     print("Engine")
